39e15253a7a4503c7d727777ac3466db335b8bdb
Alberto Miranda <codealchemist@gmail.com>
2018-12-03 08:52:35 -0300

    Fixed fullscreen.

--- src/fullscreen.js
+++ src/fullscreen.js
@@ -1,17 +1,23 @@
 class Fullscreen {
-  constructor () {
+  isFullscreen () {
     this.fullscreenElement = document.fullscreenElement
     if (this.fullscreenElement === undefined) this.fullscreenElement = document.webkitFullscreenElement
+    return !!this.fullscreenElement
   }
 
   toggle () {
-    if (!this.fullscreenElement) {
-      if (document.body.requestFullscreen) return document.body.requestFullscreen()
-      document.body.webkitRequestFullScreen()
-    } else {
-      if (document.exitFullscreen) document.exitFullscreen()
-      document.webkitExitFullscreen()
-    }
+    if (this.isFullscreen()) return this.disableFullscreen()
+    this.enableFullscreen()
+  }
+
+  enableFullscreen () {
+    if (document.body.requestFullscreen) return document.body.requestFullscreen()
+    document.body.webkitRequestFullScreen()
+  }
+
+  disableFullscreen () {
+    if (document.exitFullscreen) document.exitFullscreen()
+    document.webkitExitFullscreen()
   }
 
   set (el = document) {
